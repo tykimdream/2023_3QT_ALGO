12a9c97849b50afc64482aa0e8e9cc2458a35768
pikan <53204415+tykimdream@users.noreply.github.com>
2023-07-24 15:03:55 +0900

    [Silver II] Title: 최소 힙, Time: 1380 ms, Memory: 33232 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		PriorityQueue<Integer> que;
+
+		que = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+					return o1 - o2;
+			}
+		});
+
+		for (int i = 0; i < n; i++) {
+			int input = Integer.parseInt(br.readLine());
+			if (input == 0) {
+				if (que.size() > 0)
+					System.out.println(que.poll());
+				else
+					System.out.println(0);
+			} else {
+				que.add(input);
+			}
+		}
+	}
+}
