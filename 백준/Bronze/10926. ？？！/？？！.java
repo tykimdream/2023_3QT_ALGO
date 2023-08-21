c3c443139a61cb22002bc0f289b5716f2776bc37
pikan <53204415+tykimdream@users.noreply.github.com>
2023-08-21 13:39:29 +0900

    [Bronze V] Title: ??!, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine() + "??!");
+	}
+
+}
