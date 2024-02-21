5ae4332ea348e29f7c2903cd7a54db0964efb71e
pikan <53204415+tykimdream@users.noreply.github.com>
2024-02-21 19:29:06 +0900

    [Silver V] Title: LCM, Time: 72 ms, Memory: 11404 KB -BaekjoonHub

--- 백준/Silver/5347. LCM/LCM.java
+++ 백준/Silver/5347. LCM/LCM.java
@@ -5,26 +5,28 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int loop = Integer.parseInt(br.readLine());
-        
-        for(int i = 0; i < loop; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            
-            int GCD = getGCD(a, b);
-            long LCM = (long)a * b / GCD; // long 타입으로 형변환하여 오버플로우 방지
-            System.out.println(LCM);
-        }
-    }
-    
-    public static int getGCD(int a, int b) {
-        while (b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
-        }
-        return a;
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int loop = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < loop; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			long min = Math.min(a, b);
+			long max = Math.max(a, b);
+			
+			long GCD = getGCD(max, min);
+			long LCM = (max * min) / GCD;
+			System.out.println(LCM);
+		}
+		
+	}
+	
+	public static long getGCD(long max, long min) {
+		if(max % min == 0) {
+			return min;
+		}
+		return getGCD(min, max % min);
+	}
 }
