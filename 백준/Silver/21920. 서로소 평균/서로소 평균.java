af900d44051084796b20f0947ca604659a51dc7b
pikan <53204415+tykimdream@users.noreply.github.com>
2024-02-22 19:49:09 +0900

    [Silver IV] Title: 서로소 평균, Time: 376 ms, Memory: 66816 KB -BaekjoonHub

---
+++ 백준/Silver/21920. 서로소 평균/서로소 평균.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int x = Integer.parseInt(br.readLine());
+        double sum = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (getGCD(arr[i], x) == 1) {
+                sum += arr[i];
+                count++;
+            }
+        }
+        System.out.println(sum / count);
+    }
+
+    private static int getGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
