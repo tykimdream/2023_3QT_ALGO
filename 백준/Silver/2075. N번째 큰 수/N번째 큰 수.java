1129ae13f19bbab96d7695822ffe5ff744255dae
pikan <53204415+tykimdream@users.noreply.github.com>
2024-04-03 14:09:34 +0900

    [Silver II] Title: N번째 큰 수, Time: 848 ms, Memory: 333728 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,20 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		});
+
 		int n = Integer.parseInt(st.nextToken());
-		
-		PriorityQueue<Integer>que = new PriorityQueue<>(Collections.reverseOrder());
-		
-		for(int i = 0; i < n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int t = 0; t < n; t++) que.add(Integer.parseInt(st.nextToken()));
+			for (int j = 0; j < n; j++) {
+				pq.add(Integer.parseInt(st.nextToken()));
+			}
 		}
-		for(int i = 1; i <n ;i++) que.poll();
-		
-		System.out.println(que.poll());
+
+		for (int i = 0; i < n - 1; i++) {
+			pq.poll();
+		}
+
+		System.out.println(pq.poll());
+
 	}
+
 }
