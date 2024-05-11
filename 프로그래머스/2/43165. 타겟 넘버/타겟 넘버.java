1ad81af0effb4935ed8a7787e29a5c7588e4e2d8
pikan <53204415+tykimdream@users.noreply.github.com>
2024-05-11 23:17:20 +0900

    [level 2] Title: 타겟 넘버, Time: 0.36 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,15 @@
 class Solution {
-    static int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, -1, 0);    
-        
-        return answer;
+        return dfs(numbers, target, -1, 0);    
     }
     
-    public void dfs(int[] numbers, int target, int index, int sum){
+    public int dfs(int[] numbers, int target, int index, int sum){
         if(index == numbers.length -1 && sum == target){
-            answer++;
-            return;
+            return 1;
         }
-        if(index == numbers.length - 1) return ;
+        if(index == numbers.length - 1) return 0;
         
-        dfs(numbers,target, index + 1, sum + numbers[index + 1]);
-        dfs(numbers,target, index + 1, sum - numbers[index + 1]);
+        return dfs(numbers,target, index + 1, sum + numbers[index + 1]) + dfs(numbers,target, index + 1, sum - numbers[index + 1]);
     }
 }
