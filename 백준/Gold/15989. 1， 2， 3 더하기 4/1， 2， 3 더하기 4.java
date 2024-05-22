5dc65bf1a14e48e155fa0de0687c2d4ddb2d9ef5
pikan <53204415+tykimdream@users.noreply.github.com>
2024-05-23 00:10:32 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 88 ms, Memory: 12120 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+		
+		int[][] dp = new int[10001][4];
+		dp[1][1] = 1;
+		dp[2][1] = 1;
+		dp[2][2] = 1;
+		dp[3][1] = 1;
+		dp[3][2] = 1;
+		dp[3][3] = 1;
+
+		for(int i = 4; i <= 10000; i++) {
+			dp[i][1] = dp[i-1][1];
+			dp[i][2] = dp[i-2][1] + dp[i-2][2];
+			dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+		}
+		for (int t = 0; t < T; t++) {
+			int i = Integer.parseInt(br.readLine());
+			sb.append(dp[i][1] + dp[i][2] + dp[i][3]);
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
