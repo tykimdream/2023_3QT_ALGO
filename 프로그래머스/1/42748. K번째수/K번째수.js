09b6410f347144f137811596ddb0bd9dc7960bd1
pikan <53204415+tykimdream@users.noreply.github.com>
2024-07-07 17:04:03 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+    var answer = [];
+    
+    for(command of commands){
+        answer.push(array.slice(command[0] - 1, command[1]).sort((a, b) => a - b)[command[2] - 1])
+    }
+    return answer;
+}
